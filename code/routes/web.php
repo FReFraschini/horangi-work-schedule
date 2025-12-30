67d32a022980c30b4c55a5ca4f83253a2b36f20f
Federco Re Fraschini <federico.refraschini@gmail.com>
2025-12-30 15:25:54 +0100

    feat: rimozione default laravel e sistemazione rotte

--- code/routes/web.php
+++ code/routes/web.php
@@ -3,10 +3,10 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes(['register' => false]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/', function () {
+    return redirect('home');
+});
